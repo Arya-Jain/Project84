47dad93ad20e476b5b385057263461b2e5c1e896
Arya-Jain <89778433+Arya-Jain@users.noreply.github.com>
2021-10-18 18:20:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Create a reference for the canvas
-canvas= document.getElementById('myCanvas');
-ctx= canvas.getContext("2d");
+ var canvas= document.getElementById("myCanvas");
+ ctx.canvas.getcontext("2d");
 
 img_width = 300;
 img_height = 100;
@@ -22,18 +22,21 @@ function uploadimg() {
 }
 
 //Write a code to grab the key-pressed event
-window.addEventListener("keydown", my_keydown);
+window.addEventListener("keydown",my_keydown);
 
 function my_keydown(e)
 {
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
 	
-		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
-		//write a code to check the type of key pressed
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90)){
+        //write a code to check the type of key pressed
 		aplhabetkey();
-		document.getElementById("d1").innerHTML="You pressed Alphabet Key"
-	 else{
+		document.getElementById("d1").innerHTML="You have pressed Alphabet Key";
+		console.log("alphabet key");
+		}
+		
+	else{
 		otherkey();
 		document.getElementById("d1").innerHTML="You pressed symbol or other key";
 	}
@@ -41,25 +44,25 @@ function my_keydown(e)
 
 function aplhabetkey()
 {
-	//upload respective image with the message.
-	img_image="Alpkey.png"
-	add() ;
+	//upload respective image with the message. 
+	img_image="Alpkey.png";
+	add();
 
 }
 function numberkey()
 {
-	img_image="numkey.png"
-	add() ;	
+	img_image="numkey.png";
+	add();
 }
 function arrowkey()
 {
-	img_image="Arrkey.png"
-	add() ;
+	img_image="Arrkey.png";
+	add();
 }
 function specialkey()
 {
-	img_image="spkey.png"
-	add() ;	
+	img_image="spkey.png";
+	add();	
 }
 function otherkey()
 {
